81a9a24d069d8c2152fc426bb5d9d49300a844c8
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-12-04 15:45:18 +0200

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello DocDoc Booking Application</Text>
+      <Text style={styles.text}>Hello Clinic Management Application</Text>
     </View>
   );
 };
