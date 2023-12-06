28da2c3f90d3c158d086308f26ad42f0aadbcbad
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-12-06 21:20:15 +0200

    App Starting Structure

---
+++ src/components/AppMainButton/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './styles';
+
+const AppMainButton = () => {
+  return (
+    <View style={styles.container}>
+      <Text>AppMainButton</Text>
+    </View>
+  );
+};
+
+export default AppMainButton;
---
+++ src/utils/Theme.js
@@ -0,0 +1,47 @@
+export const COLORS = {
+  primaryColor: '##247CFF',
+};
+
+export const BORDERRADIUS = {
+  radius_4: 4,
+  radius_8: 8,
+  radius_10: 10,
+  radius_12: 12,
+  radius_14: 14,
+  radius_16: 16,
+  radius_18: 18,
+  radius_20: 20,
+  radius_22: 22,
+  radius_24: 24,
+  radius_26: 26,
+};
+
+export const FONTFAMILY = {
+  //! We Will add our app fonts family here
+  //   poppins_black: 'Poppins-Black',
+  //   poppins_bold: 'Poppins-Bold',
+  //   poppins_extrabold: 'Poppins-ExtraBold',
+  //   poppins_extralight: 'Poppins-ExtraLight',
+  //   poppins_light: 'Poppins-Light',
+  //   poppins_medium: 'Poppins-Medium',
+  //   poppins_regular: 'Poppins-Regular',
+  //   poppins_semibold: 'Poppins-SemiBold',
+  //   poppins_thin: 'Poppins-Thin',
+};
+
+export const FONTSIZE = {
+  size_4: 4,
+  size_8: 8,
+  size_10: 10,
+  size_12: 12,
+  size_14: 14,
+  size_16: 16,
+  size_18: 18,
+  size_20: 20,
+  size_22: 22,
+  size_24: 24,
+  size_26: 26,
+  size_28: 28,
+  size_30: 30,
+  size_32: 32,
+};
